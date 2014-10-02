5c7b1920405bb479025343730c00ea45b03f84a2
LanBangjue <25112391@qq.com>
2014-10-02 19:25:12 +0800

    141002

---
+++ README.md
@@ -0,0 +1,13 @@
+smartImg
+=======
+
+		响应式图片处理工具
+		author:[VaJoy Larn](http://www.cnblogs.com/vajoy/)<br />
+### 使用方法
+### 在 img 标签中添加 s-src="图片地址1 屏宽1,图片地址2 屏宽2, ..., 图片地址n 屏宽n," ，若屏宽参数没写则默认为0
+### 如下例子：
+		<img s-src="img/large.jpg 900,img/huge.jpg 1100,img/small.jpg" />
+### 它表示当屏幕宽度大于1100px时显示img/huge.jpg，当屏幕宽度小于1100px但大于900px时显示img/large.jpg，其它更小屏幕宽度时显示img/small.jpg
+### 当然，如果图片地址前缀是一样的，我们还可以加上s-prefix属性：
+		<img s-prefix="img/" s-src="large.jpg 900,huge.jpg 1200,middle.jpg 650,small.jpg 0" />
+### 查看效果请运行demo.html
---
+++ smartImg.js
@@ -0,0 +1,50 @@
+/**
+ * Created by VaJoy on 2014/10/2.
+ * based On jQuery
+ * @author VaJoy Larn
+ * https://github.com/VaJoy/smartImg
+ * http://vajoy.cnblogs.com
+ * Do not delete here while you are using smartImg
+ */
+(function($) {
+    var st = null;
+    $.smartImg = function() {
+        var $Imgs = $("img[s-src]");
+        if ($Imgs.length) {
+            var $img, prefix, srcs, i = 0, srca, srcb, url, screen_w;
+            var win_w = $(window).width();
+            var exp = /^.*\.[^\d]*/g;
+            $Imgs.each(function (i, e) {
+                $img = $(this);
+                prefix = $img.attr("s-prefix") ? $img.attr("s-prefix") : "";
+                srcs = $img.attr("s-src").split(",").sort(function (a, b) {
+                    srca = a.replace(exp, "");
+                    srcb = b.replace(exp, "");
+                    if (srca && srcb) return srcb - srca; //防止没写屏宽数值
+                    else return 0;
+                });
+
+                i=0;
+                while (i < srcs.length) {
+                    screen_w = srcs[i].replace(exp, "");
+                    screen_w = screen_w?screen_w:0; //防止没写屏宽数值
+                    if (win_w > screen_w) {
+                        url = prefix + exp.exec(srcs[i])[0];
+                        $img.attr("src", url);
+                        break;
+                    }
+                    i++;
+                }
+            })
+
+            $(window).resize(function(){
+                clearTimeout(st);
+                st = setTimeout($.smartImg,100);
+            })
+
+        }
+    }
+    $.smartImg();
+})(jQuery);
+
+
